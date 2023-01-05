f20adf154efd27b740ab4749ce879fe228a6370d
Utsob Roy <roy@utsob.me>
2023-01-05 15:57:37 +0600

    theme linking instead of embedding

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -1,39 +1,44 @@
 require("dotenv").config();
 const axios = require("axios");
+const fs = require("fs");
 
-
-module.exports = async() => {
-    let themeStyle = "";
-    let themeUrl = process.env.THEME;
-    if (themeUrl) {
-        //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
-        //Not all themes with no legacy mark have a theme.css file, so we need to check for it
-        try{
-            await axios.get(themeUrl);
-        }catch{
-            if(themeUrl.indexOf("theme.css") > -1){
-                themeUrl = themeUrl.replace("theme.css", "obsidian.css");
-            }
-            else if(themeUrl.indexOf("obsidian.css") > -1){
-                themeUrl = themeUrl.replace("obsidian.css", "theme.css");
-            }
-        }
-
-        const res = await axios.get(themeUrl);
-        themeStyle = `<style>${res.data}</style>`;
+module.exports = async () => {
+  let themeStyle = "";
+  let themeUrl = process.env.THEME;
+  if (themeUrl) {
+    //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
+    //Not all themes with no legacy mark have a theme.css file, so we need to check for it
+    try {
+      await axios.get(themeUrl);
+    } catch {
+      if (themeUrl.indexOf("theme.css") > -1) {
+        themeUrl = themeUrl.replace("theme.css", "obsidian.css");
+      } else if (themeUrl.indexOf("obsidian.css") > -1) {
+        themeUrl = themeUrl.replace("obsidian.css", "theme.css");
+      }
     }
-    let baseUrl = process.env.SITE_BASE_URL || "";
-    if(baseUrl && !baseUrl.startsWith("http")){
-        baseUrl = "https://" + baseUrl;
-    }
-    const meta ={
-        env: process.env.ELEVENTY_ENV,
-        theme: process.env.THEME,
-        themeStyle: themeStyle,
-        baseTheme: process.env.BASE_THEME || "dark",
-        siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
-        siteBaseUrl: baseUrl,
-    };
 
-    return meta;
+    const res = await axios.get(themeUrl);
+    fs.writeFileSync("dist/styles/theme.css", res.data, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("File saved");
+    });
+    themeStyle = "/styles/theme.css";
+  }
+  let baseUrl = process.env.SITE_BASE_URL || "";
+  if (baseUrl && !baseUrl.startsWith("http")) {
+    baseUrl = "https://" + baseUrl;
+  }
+  const meta = {
+    env: process.env.ELEVENTY_ENV,
+    theme: process.env.THEME,
+    themeStyle: themeStyle,
+    baseTheme: process.env.BASE_THEME || "dark",
+    siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
+    siteBaseUrl: baseUrl,
+  };
+
+  return meta;
 };
