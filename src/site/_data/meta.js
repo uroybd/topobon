d50dbf094183750e8d8999d4d5c238ba253710bf
Utsob Roy <roy@utsob.me>
2023-01-06 12:54:27 +0600

    refactored theme generation

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -1,32 +1,7 @@
 require("dotenv").config();
-const axios = require("axios");
-const fs = require("fs");
 
 module.exports = async () => {
-  let themeStyle = "";
-  let themeUrl = process.env.THEME;
-  if (themeUrl) {
-    //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
-    //Not all themes with no legacy mark have a theme.css file, so we need to check for it
-    try {
-      await axios.get(themeUrl);
-    } catch {
-      if (themeUrl.indexOf("theme.css") > -1) {
-        themeUrl = themeUrl.replace("theme.css", "obsidian.css");
-      } else if (themeUrl.indexOf("obsidian.css") > -1) {
-        themeUrl = themeUrl.replace("obsidian.css", "theme.css");
-      }
-    }
-
-    const res = await axios.get(themeUrl);
-    fs.writeFileSync("src/site/styles/theme.css", res.data, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("File saved");
-    });
-    themeStyle = "/styles/theme.css";
-  }
+  themeStyle = "/styles/theme.css";
   let baseUrl = process.env.SITE_BASE_URL || "";
   if (baseUrl && !baseUrl.startsWith("http")) {
     baseUrl = "https://" + baseUrl;
