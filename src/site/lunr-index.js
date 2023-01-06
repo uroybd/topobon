72285c7d5b46fa690e0b531ea3f4c09da3651acb
Utsob Roy <roy@utsob.me>
2023-01-06 14:16:33 +0600

    Update file src/site/lunr-index.js

--- src/site/lunr-index.js
+++ src/site/lunr-index.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-const lunrjs = require("lunr");
-const path = require("path");
+require("dotenv").config()
+const lunrjs = require('lunr');
+const path = require('path');
 
 function createIndex(posts) {
   return lunrjs(function () {
-    this.ref("id");
-    this.field("title");
-    this.field("content");
-    this.field("date");
-    this.field("tags");
+    this.ref('id');
+    this.field('title');
+    this.field('content');
+    this.field('date');
 
     posts.forEach((p, idx) => {
       p.id = idx;
@@ -17,9 +16,6 @@ function createIndex(posts) {
   });
 }
 
-const data = require("../../netlify/functions/search/data.json");
+const data = require('../../netlify/functions/search/data.json');
 const index = createIndex(data);
-require("fs").writeFileSync(
-  path.join(__dirname, "../../netlify/functions/search/index.json"),
-  JSON.stringify(index)
-);
+require('fs').writeFileSync(path.join(__dirname, '../../netlify/functions/search/index.json'), JSON.stringify(index));
