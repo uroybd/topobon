ce5687dcbe7b79984613d60be9ec6cc377a71639
Utsob Roy <roy@utsob.me>
2023-01-20 00:30:55 +0600

    dynamic theme

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
+const axios = require("axios");
+const fs = require("fs");
+const crypto = require("crypto");
+const glob = require("glob");
 
 module.exports = async () => {
-  themeStyle = "/styles/theme.css";
   let baseUrl = process.env.SITE_BASE_URL || "";
   if (baseUrl && !baseUrl.startsWith("http")) {
     baseUrl = "https://" + baseUrl;
   }
+  let themeStyle = glob.sync("src/site/styles/theme.*.css")[0] || "";
+  if (themeStyle) {
+    themeStyle = themeStyle.split("site")[1];
+  }
   const meta = {
     env: process.env.ELEVENTY_ENV,
     theme: process.env.THEME,
-    themeStyle: themeStyle,
+    themeStyle,
     baseTheme: process.env.BASE_THEME || "dark",
     siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
     siteBaseUrl: baseUrl,
--- src/site/get-theme.js
+++ src/site/get-theme.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs");
+const crypto = require("crypto");
+const glob = require("glob");
 
-async function getTheme(){
+async function getTheme() {
   let themeUrl = process.env.THEME;
   if (themeUrl) {
     //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
@@ -18,15 +20,20 @@ async function getTheme(){
     }
 
     const res = await axios.get(themeUrl);
-    fs.writeFileSync("src/site/styles/theme.css", res.data, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("File saved");
-    });
-    
+    try {
+      const existing = glob.sync("src/site/styles/theme.*.css");
+      existing.forEach((file) => {
+        fs.rmSync(file);
+      });
+    } catch {}
+    const hashSum = crypto.createHash("sha256");
+    hashSum.update(res.data);
+    const hex = hashSum.digest("hex");
+    fs.writeFileSync(
+      `src/site/styles/theme.${hex.substring(0, 8)}.css`,
+      res.data
+    );
   }
-};
-
+}
 
 getTheme();
