8f0c5efdb09d58a5b40cf99a9d8208f2cd10e4d1
Utsob Roy <roy@utsob.me>
2023-01-26 10:22:29 +0600

    netlify redirect

--- .eleventy.js
+++ .eleventy.js
@@ -251,6 +251,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/site/img");
+  eleventyConfig.addPassthroughCopy("src/site/_redirects");
   eleventyConfig.addPlugin(faviconPlugin, { destination: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
