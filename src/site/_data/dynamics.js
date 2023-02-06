00f59a339e7bf7ec2e6e1de511ff1e8d4958df97
Utsob Roy <roy@utsob.me>
2023-02-06 14:25:47 +0600

    dynamic import

--- src/site/_data/dynamics.js
+++ src/site/_data/dynamics.js
@@ -1,21 +1,33 @@
 const fsFileTree = require("fs-file-tree");
 
 const BASE_PATH = "src/site/_includes/components/user"
-const NOTES_PATH = `${BASE_PATH}/notes`
-const COMMON_PATH = `${BASE_PATH}/common`
+const NAMESPACES = ["index", "notes", "common"];
+const SLOTS = ["header", "afterContent", "footer"]
 
-module.exports = async () => {
-    const note_comp_tree = await fsFileTree(`${NOTES_PATH}/footers`);
-    let note_footers = Object.keys(note_comp_tree).filter((p) => p.indexOf(".njk") != -1).map((p) => `components/user/notes/footers/${p}`);
-    
-    const common_comp_tree = await fsFileTree(`${COMMON_PATH}/footers`);
-    let common_footers = Object.keys(common_comp_tree).filter((p) => p.indexOf(".njk") != -1).map((p) => `components/user/common/footers/${p}`);
-    return {
-        notes: {
-            footers: note_footers,
-        },
-        common: {
-            footers: common_footers
+const generateComponentPaths = async (namespace) => {
+    const data = {};
+    for (let index = 0; index < SLOTS.length; index++) {
+        const slot = SLOTS[index];
+        try {
+            console.log(`${BASE_PATH}/${namespace}/${slot}`);
+            const tree = await fsFileTree(`${BASE_PATH}/${namespace}/${slot}`);
+            let comps = Object.keys(tree).filter((p) => p.indexOf(".njk") != -1).map((p) => `components/user/${namespace}/${slot}/${p}`);
+            comps.sort()
+            data[slot] = comps;
+        } catch {
+            data[slot] = [];
         }
     }
+    return data;
+}
+
+
+module.exports = async () => {
+    const data = {};
+    for (let index = 0; index < NAMESPACES.length; index++) {
+        const ns = NAMESPACES[index];
+        data[ns] = await generateComponentPaths(ns);
+    }
+    console.log(data);
+    return data;
 }
