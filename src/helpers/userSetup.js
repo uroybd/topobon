768a666fb6e72e1617d7d0ae9a0abadac5ca9246
Utsob Roy <roy@utsob.me>
2023-03-27 10:10:16 +0600

    image optimizations

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,5 +1,29 @@
 const { parse } = require("node-html-parser");
 
+const Image = require("@11ty/eleventy-img");
+function transformImage(src, cls, alt, sizes, widths = ["500", "700", "auto"]) {
+  let options = {
+    widths: widths,
+    formats: ["webp", "jpeg"],
+    outputDir: "./dist/img/optimized",
+    urlPath: "/img/optimized",
+  };
+
+  // generate images, while this is async we don’t wait
+  Image(src, options);
+
+  let imageAttributes = {
+    class: cls,
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+  // get metadata even if the images are not fully generated yet
+  let metadata = Image.statsSync(src, options);
+  return metadata;
+}
+
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
@@ -14,6 +38,54 @@ function userEleventySetup(eleventyConfig) {
       t.tagName = "div";
       t.classList.add("table-wrapper");
       t.innerHTML = `<table>${inner}</table>`;
+    }
+    return str && parsed.innerHTML;
+  });
+
+  eleventyConfig.addTransform("picture", function (str) {
+    const parsed = parse(str);
+    for (const t of parsed.querySelectorAll(".cm-s-obsidian img")) {
+      // try {
+
+      // } catch {}
+      const src = t.getAttribute("src");
+      const cls = t.classList;
+      const alt = t.getAttribute("alt");
+      if (src && src.startsWith("/") && !src.endsWith(".svg")) {
+        const meta = transformImage(
+          "./src/site" + decodeURI(t.getAttribute("src")),
+          cls.toString(),
+          alt,
+          ["(max-width: 480px)", "(max-width: 1024px)"]
+        );
+
+        if (meta) {
+          t.tagName = "picture";
+          t.innerHTML = `<source
+      media="(max-width:480px)"
+      srcset="${meta.webp[0].url}"
+      type="image/webp"
+    />
+    <source
+      media="(max-width:480px)"
+      srcset="${meta.jpeg[0].url}"
+    />
+    <source
+      media="(max-width:1920px)"
+      srcset="${meta.webp[1].url}"
+      type="image/webp"
+    />
+    <source
+      media="(max-width:1920px)"
+      srcset="${meta.jpeg[1].url}"
+    />
+    <img
+      class="${cls.toString()}"
+      src="${src}"
+      alt="${alt}"
+    />`;
+        }
+      }
     }
     return str && parsed.innerHTML;
   });
