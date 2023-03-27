4d54cdb8ed02872264add84a799db0a7e5b1880c
Utsob Roy <roy@utsob.me>
2023-03-27 10:58:21 +0600

    fault tolarent image gen

--- .eleventy.js
+++ .eleventy.js
@@ -187,7 +187,10 @@ module.exports = function (eleventyConfig) {
           if (frontMatter.data.permalink) {
             permalink = frontMatter.data.permalink;
           }
-          if (frontMatter.data.tags && frontMatter.data.tags.indexOf("gardenEntry") != -1) {
+          if (
+            frontMatter.data.tags &&
+            frontMatter.data.tags.indexOf("gardenEntry") != -1
+          ) {
             permalink = "/";
           }
           if (frontMatter.data.noteIcon) {
@@ -254,7 +257,7 @@ module.exports = function (eleventyConfig) {
         let calloutType = "";
         let isCollapsable;
         let isCollapsed;
-        const calloutMeta = /\[!([\w-]*)\](\+|\-){0,1}(\s?.*)/;;
+        const calloutMeta = /\[!([\w-]*)\](\+|\-){0,1}(\s?.*)/;
         if (!content.match(calloutMeta)) {
           continue;
         }
@@ -322,12 +325,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss, {
     posthtmlRenderOptions: {
       closingSingleTag: "slash",
-      singleTags: ["link"]
-    }  
+      singleTags: ["link"],
+    },
   });
 
-  eleventyConfig.addFilter("dateToZulu", function(date){
-    if(!date) return "";
+  eleventyConfig.addFilter("dateToZulu", function (date) {
+    if (!date) return "";
     return new Date(date).toISOString("dd-MM-yyyyTHH:mm:ssZ");
   });
   eleventyConfig.addFilter("jsonify", function (variable) {
