ad7c4197b8e85b30818ba975276da49166a529e8
Utsob Roy <roy@utsob.me>
2023-07-11 11:19:18 +0600

    Update file .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const slugify = require("@sindresorhus/slugify");
 const markdownIt = require("markdown-it");
 const fs = require("fs");
 const matter = require("gray-matter");
-const faviconPlugin = require("eleventy-favicon");
+const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 const tocPlugin = require("eleventy-plugin-nesting-toc");
 const { parse } = require("node-html-parser");
 const htmlMinifier = require("html-minifier");
@@ -38,6 +38,7 @@ module.exports = function (eleventyConfig) {
   let markdownLib = markdownIt({
     breaks: true,
     html: true,
+    linkify: true,
   })
     .use(require("markdown-it-anchor"), {
       slugify: headerToId,
@@ -90,30 +91,55 @@ module.exports = function (eleventyConfig) {
         }
         if (token.info.startsWith("ad-")) {
           const code = token.content.trim();
-          if (code && code.toLowerCase().startsWith("title:")) {
-            const title = code.substring(6, code.indexOf("\n"));
-            const titleDiv = title
-              ? `<div class="callout-title"><div class="callout-title-inner">${title}</div></div>`
+          const parts = code.split("\n")
+          let titleLine;
+          let collapse;
+          let collapsible = false
+          let collapsed = true
+          let icon;
+          let color;
+          let nbLinesToSkip = 0
+          for (let i = 0; i<4; i++) {
+            if (parts[i] && parts[i].trim()) {
+              let line = parts[i] && parts[i].trim().toLowerCase()
+              if (line.startsWith("title:")) {
+                titleLine = line.substring(6);
+                nbLinesToSkip ++;
+              } else if (line.startsWith("icon:")) {
+                icon = line.substring(5);
+                nbLinesToSkip ++;
+              } else if (line.startsWith("collapse:")) {
+                collapsible = true
+                collapse = line.substring(9);
+                if (collapse && collapse.trim().toLowerCase() == 'open') {
+                  collapsed = false
+                }
+                nbLinesToSkip ++;
+              } else if (line.startsWith("color:")) {
+                color = line.substring(6);
+                nbLinesToSkip ++;
+              }
+            }
+          }
+          const foldDiv = collapsible ? `<div class="callout-fold">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-chevron-down">
+              <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
+          </div>` : "";
+          const titleDiv = titleLine
+              ? `<div class="callout-title"><div class="callout-title-inner">${titleLine}</div>${foldDiv}</div>`
               : "";
-
-            return `<div class="callout" data-callout="${
-              token.info
-            }">${titleDiv}\n<div class="callout-content">${md.render(
-              code.slice(code.indexOf("\n"))
-            )}</div></div>`;
+          let collapseClasses = titleLine && collapsible ? 'is-collapsible' : ''
+          if (collapsible && collapsed) {
+            collapseClasses += " is-collapsed"
           }
 
-          const title = `<div class="callout-title"><div class="callout-title-inner">${token.info
-            .charAt(3)
-            .toUpperCase()}${token.info
-            .substring(4)
-            .toLowerCase()}</div></div>`;
-
-          return `<div class="callout" data-callout="${
-            token.info
-          }">${title}\n<div class="callout-content">${md.render(
-            code
+          let res = `<div data-callout-metadata class="callout ${collapseClasses}" data-callout="${
+            token.info.substring(3)
+          }">${titleDiv}\n<div class="callout-content">${md.render(
+            parts.slice(nbLinesToSkip).join("\n")
           )}</div></div>`;
+          return res
         }
 
         // Other languages
@@ -169,6 +195,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setLibrary("md", markdownLib);
 
+  eleventyConfig.addFilter("isoDate", function (date) {
+    return date && date.toISOString();
+  });
+
+
   eleventyConfig.addFilter("link", function (str) {
     return (
       str &&
@@ -215,9 +246,10 @@ module.exports = function (eleventyConfig) {
           deadLink = true;
         }
 
-        return `<a class="internal-link ${
-          deadLink ? "is-unresolved" : ""
-        }" ${deadLink ? "" : 'data-note-icon="' + noteIcon + '"'} href="${permalink}${headerLinkPath}">${title}</a>`;
+        if(deadLink){
+          return `<a class="internal-link is-unresolved" href="/404">${title}</a>`;
+        }
+        return `<a class="internal-link" data-note-icon="${noteIcon}" href="${permalink}${headerLinkPath}">${title}</a>`;
       })
     );
   });
@@ -311,47 +343,60 @@ module.exports = function (eleventyConfig) {
     return str && parsed.innerHTML;
   });
 
+  function fillPictureSourceSets(src, cls, alt, meta, width, imageTag) {
+      imageTag.tagName = "picture";
+      let html = `<source
+      media="(max-width:480px)"
+      srcset="${meta.webp[0].url}"
+      type="image/webp"
+      />
+      <source
+      media="(max-width:480px)"
+      srcset="${meta.jpeg[0].url}"
+      />
+      `
+      if (meta.webp && meta.webp[1] && meta.webp[1].url) {
+        html += `<source
+        media="(max-width:1920px)"
+        srcset="${meta.webp[1].url}"
+        type="image/webp"
+        />`
+      }
+      if (meta.jpeg && meta.jpeg[1] && meta.jpeg[1].url) {
+        html += `<source
+        media="(max-width:1920px)"
+        srcset="${meta.jpeg[1].url}"
+        />`
+      }
+      html += `<img
+      class="${cls.toString()}"
+      src="${src}"
+      alt="${alt}"
+      width="${width}"
+      />`;
+      imageTag.innerHTML = html;
+    }
+    
+
   eleventyConfig.addTransform("picture", function (str) {
     const parsed = parse(str);
-    for (const t of parsed.querySelectorAll(".cm-s-obsidian img")) {
-      const src = t.getAttribute("src");
+    for (const imageTag of parsed.querySelectorAll(".cm-s-obsidian img")) {
+      const src = imageTag.getAttribute("src");
       if (src && src.startsWith("/") && !src.endsWith(".svg")) {
-        const cls = t.classList;
-        const alt = t.getAttribute("alt");
+        const cls = imageTag.classList.value;
+        const alt = imageTag.getAttribute("alt");
+        const width = imageTag.getAttribute("width") || '';
 
         try {
           const meta = transformImage(
-            "./src/site" + decodeURI(t.getAttribute("src")),
+            "./src/site" + decodeURI(imageTag.getAttribute("src")),
             cls.toString(),
             alt,
             ["(max-width: 480px)", "(max-width: 1024px)"]
           );
 
           if (meta) {
-            t.tagName = "picture";
-            t.innerHTML = `<source
-      media="(max-width:480px)"
-      srcset="${meta.webp[0].url}"
-      type="image/webp"
-    />
-    <source
-      media="(max-width:480px)"
-      srcset="${meta.jpeg[0].url}"
-    />
-    <source
-      media="(max-width:1920px)"
-      srcset="${meta.webp[1].url}"
-      type="image/webp"
-    />
-    <source
-      media="(max-width:1920px)"
-      srcset="${meta.jpeg[1].url}"
-    />
-    <img
-      class="${cls.toString()}"
-      src="${src}"
-      alt="${alt}"
-    />`;
+            fillPictureSourceSets(src, cls, alt, meta, width, imageTag);
           }
         } catch {
           // Make it fault tolarent.
@@ -375,12 +420,12 @@ module.exports = function (eleventyConfig) {
     )) {
       t.classList.add("dataview");
       t.classList.add("table-view-table");
-      t.querySelector("thead").classList.add("table-view-thead");
-      t.querySelector("tbody").classList.add("table-view-tbody");
-      t.querySelectorAll("thead > tr").forEach((tr) => {
+      t.querySelector("thead")?.classList.add("table-view-thead");
+      t.querySelector("tbody")?.classList.add("table-view-tbody");
+      t.querySelectorAll("thead > tr")?.forEach((tr) => {
         tr.classList.add("table-view-tr-header");
       });
-      t.querySelectorAll("thead > tr > th").forEach((th) => {
+      t.querySelectorAll("thead > tr > th")?.forEach((th) => {
         th.classList.add("table-view-th");
       });
     }
@@ -408,7 +453,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
-  eleventyConfig.addPlugin(faviconPlugin, { destination: "dist" });
+  eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
     tags: ["h1", "h2", "h3", "h4", "h5", "h6"],
